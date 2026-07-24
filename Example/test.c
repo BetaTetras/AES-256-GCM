c4a9f98818d7bb92cc3a2c9b236e4df71cbbb687
Greg <gibrat.gregoire@gmail.com>
2026-07-25 01:37:19 +0200

    Implémentation final de AES-256-GCM

--- Example/test.c
+++ Example/test.c
@@ -10,16 +10,18 @@ int main(){
     uint8_t Cle[32];
 
     //Génération de la cle depuis une string (mot de passe)
-    StringToKeyConverter("Password123",&Cle);
+    // StringToKeyConverter("Password123",&Cle);
 
     // Chiffrement du fichier nommée "test.txt" avec la cle générée
-    AES_Encrypt_File("./Subaru.txt",Cle);
+    // AES_Encrypt_File("./Subaru.txt",Cle);
+    // AES_GCM_Encrypt_File("./Subaru.txt",Cle);
 
     // Le fichier est alord chiffrée de facon sécurisée est ne peut 
     // se faire déchiffrée uniquement grace a la clé générée ou pas
     // le mot de passe qui a permis de crée la clé de chiffrement 
 
     // AES_Decrypt_File("./Subaru.txt",Cle);
+    // AES_GCM_Decrypt_File("./Subaru.txt",Cle);
     
     return 0;
 }
